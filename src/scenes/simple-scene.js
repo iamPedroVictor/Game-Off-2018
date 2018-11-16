bfe13d731d4fdfef2f317995d1689aea3d09d757
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2018-11-15 22:14:26 -0400

    Test with 20 entities in scene

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,3 +1,5 @@
+import {Entity} from '../ECS/Entity';
+
 export class SimpleScene extends Phaser.Scene {
 
   preload(){
@@ -6,6 +8,13 @@ export class SimpleScene extends Phaser.Scene {
 
   create() {
     this.add.text(100, 100, 'Hello Pedro!', { fill: '#0f0' });
+    console.log("eae");
     this.add.image(100,200,'cokecan');
+    const entities = [];
+    for(let i = 0; i < 15 ; i++){
+      let entity = new Entity();
+      entity.print();
+      entities.push(entity);
+    }
   }
 }
